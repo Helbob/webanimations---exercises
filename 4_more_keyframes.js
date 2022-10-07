b541136768d8bbafad99b2adfd9595e4722121af
Helbob <millehelbo@outlook.dk>
2022-10-07 08:54:56 +0200

    push

--- 4_more_keyframes.js
+++ 4_more_keyframes.js
@@ -10,8 +10,8 @@ const keyframes = [
   { transform: "translate(85vw, 20vw)" },
   { transform: "translate(42vw, 41vw" },
   { transform: "translate(0vw, 20vw" },
-  { transform: "translate(42vw, 0vw)" },
 ];
-
+//Instead of repeating the first keyframe again, you can push it.
+keyframes.push(keyframes[0]);
 const ball = document.querySelector("#ball");
 const animation = ball.animate(keyframes, properties);
